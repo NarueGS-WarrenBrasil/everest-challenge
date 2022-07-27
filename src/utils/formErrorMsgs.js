e56cd9c85aa6886d631fede84915df90da3674e9
Naruê G. Santos <narue.goncalves@warren.com.br>
2022-07-27 19:30:21 -0300

    feat: add error mesages

---
+++ src/utils/formErrorMsgs.js
@@ -0,0 +1,9 @@
+export default{
+    blank: "Preencha esse campo",
+    invalidEmail: "Email inválido",
+    diferent: "Os emails devem ser iguais",
+    invalidCPF: "CPF inválido",
+    invalidDate: "Data inválida",
+    invalidPhone: "Número de telefone inválido",
+    invalidName:"Nome inválido",
+    }
